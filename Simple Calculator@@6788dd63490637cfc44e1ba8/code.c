b880a4285310666adf2ee2b8ec41ff37f639331a
Moksha <moksha1378.becse24@chitkara.edu.in>
2025-02-12 15:08:28 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -10,12 +10,11 @@ int main(){
         if(c=='-')
            printf("%d",a-b);
         else{
-            if(c=='*')
+            if(c=='*') 
                printf("%d",a*b);
             else
                printf("%d",a/b);
         }
     }
-
     return 0;
-}
+} 
